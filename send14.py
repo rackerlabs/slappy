17492a9b4d90a7af7607cf2808bae1a9add85851
TimSimmons <tim.simmons@rackspace.com>
2015-09-21 14:27:45 -0500

    test file changes

--- send14.py
+++ send14.py
@@ -17,19 +17,27 @@ CREATE = 65282
 DELETE = 65283
 
 try:
-    cre = dns.message.make_query('poo.com', CREATE, rdclass=ClassCC)
+    cre = dns.message.make_query('example1.com', CREATE, rdclass=ClassCC)
     cre.set_opcode(14)
     cre.flags -= dns.flags.RD
 
-    response = dns.query.udp(cre, '127.0.0.1', port=8053, timeout=5)
+    response = dns.query.udp(cre, '127.0.0.1', port=5358, timeout=5)
 except Exception:
     pass
 
-try:
-    dell = dns.message.make_query('poo.com', DELETE, rdclass=ClassCC)
-    dell.set_opcode(14)
-    dell.flags -= dns.flags.RD
+# try:
+#     dell = dns.message.make_query('example1.com', DELETE, rdclass=ClassCC)
+#     dell.set_opcode(14)
+#     dell.flags -= dns.flags.RD
 
-    response = dns.query.udp(dell, '127.0.0.1', port=8053, timeout=5)
-except Exception:
-    pass
+#     response = dns.query.udp(dell, '127.0.0.1', port=5358, timeout=5)
+# except Exception:
+#     pass
+
+# import dns.resolver
+
+# resolver = dns.resolver.Resolver()
+# resolver.nameservers = ['127.0.0.1:5358']
+# answers = dns.resolver.query('example1.com', 'A')
+# for rdata in answers:
+#     print rdata
