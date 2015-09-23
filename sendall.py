ee8f3a220edf8aea5c63d7d8fc02677b490d85af
TimSimmons <tim.simmons@rackspace.com>
2015-09-23 10:27:48 -0500

    Some test file updates

--- sendall.py
+++ sendall.py
@@ -4,6 +4,7 @@ import dns.opcode
 import dns.flags
 import dns.query
 
+
 # Command and Control OPCODE
 CC = 14
 
@@ -16,25 +17,21 @@ FAILURE = 65281
 CREATE = 65282
 DELETE = 65283
 
-
 cre = dns.message.make_query('example1.com', CREATE, rdclass=ClassCC)
 cre.set_opcode(14)
 cre.flags -= dns.flags.RD
 
-response = dns.query.udp(cre, '127.0.0.1', port=5358, timeout=5)
+response = dns.query.tcp(cre, '127.0.0.1', port=5358, timeout=5)
+
+notify = dns.message.make_query('example1.com', dns.rdatatype.SOA)
+notify.set_opcode(dns.opcode.NOTIFY)
+notify.flags -= dns.flags.RD
+
+response = dns.query.tcp(notify, '127.0.0.1', port=5358, timeout=5)
 
 
 dell = dns.message.make_query('example1.com', DELETE, rdclass=ClassCC)
 dell.set_opcode(14)
 dell.flags -= dns.flags.RD
 
-response = dns.query.udp(dell, '127.0.0.1', port=5358, timeout=5)
-
-
-# import dns.resolver
-
-# resolver = dns.resolver.Resolver()
-# resolver.nameservers = ['127.0.0.1:5358']
-# answers = dns.resolver.query('example1.com', 'A')
-# for rdata in answers:
-#     print rdata
+response = dns.query.tcp(dell, '127.0.0.1', port=5358, timeout=5)
